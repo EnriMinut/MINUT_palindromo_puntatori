e3713fa1769a9d1f52bacf54082d25858fd2d398
Enriminut08 <12999495@itisrossi.vi.it>
2025-10-08 11:30:22 +0200

    Cicli while

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+    char palindromo[] = "i topi non avevano nipoti";
+    char *inizio = palindromo;
+    char *fine = palindromo;
+
+    while (*fine != '\0') {
+        fine++;
+    }
+    fine--; // Puntatore all'ultimo carattere
+
+    while (*inizio <= *fine) {
+        if (*inizio == ' ') {
+            inizio++;
+        }
+        if (*fine == ' ') {
+            fine --;
+        }
+    }
+
+
 }
